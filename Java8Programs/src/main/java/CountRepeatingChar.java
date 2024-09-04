97abffa8e46d18f2a69dcbd3af95a06477d57fc1
shaj <shajashwin@gmail.com>
2024-09-04 16:52:19 +0530

    Java 8 code added

---
+++ Java8Programs/src/main/java/CountRepeatingChar.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountRepeatingChar {
+    public static void main(String[] args) {
+
+        String line = "cloudysunday";
+
+        String lineArray[] = line.split("");
+
+
+        Map<String, Long> map = new HashMap<>();
+
+
+                map = Arrays.stream(lineArray)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+
+
+        System.out.println(map);
+
+//Another method
+
+        String lineArray1[] = line.split("");
+        Map<String, Integer> mapp = new HashMap<>();
+
+        Arrays.stream(lineArray1)
+                .forEach(s -> mapp.put(s,mapp.getOrDefault(s,0)+1));
+
+        System.out.println(mapp);
+
+    }
+}
---
+++ Java8Programs/src/main/java/FindRepeatingChar.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindRepeatingChar {
+    public static void main(String[] args) {
+
+        String str = "bestsincedayone";
+
+        String[] chars = str.split("");
+
+        List<String> entries = Arrays.stream(chars)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .filter(s->s.getValue()>1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+
+        System.out.println(entries);
+
+    }
+}
