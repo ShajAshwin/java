e448b9e09b407eae1cf613d414a8f97091aa51e6
shaj <shajashwin@gmail.com>
2025-02-16 08:28:22 +0530

    updated

--- Java8Programs/src/main/java/java8/FindLargestString.java
+++ Java8Programs/src/main/java/java8/FindLargestString.java
@@ -1,12 +1,10 @@
 package java8;
 
 import java.util.Arrays;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class FindLargestString {
     public static void main(String[] args) {
-        String string[] = {"java","spring","springBoot","microservices"};
+        String string[] = {"java","spring", "springBoot.txt","microservices"};
 
         String largestString = Arrays.stream(string)
                 .reduce((a,b) -> a.length()<b.length()?a:b)
--- Java8Programs/src/main/java/practice/Practice.java
+++ Java8Programs/src/main/java/practice/Practice.java
@@ -1,5 +1,33 @@
 package practice;
 
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Practice {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,15,8,49,25,98,32,4,8,15);
+        List<Integer> list2 = Arrays.asList(1,3,5,7,8,9,2,3,8);
+        List<String> names = Arrays.asList("AA", "BB", "AA", "CC");
+        int arr[] = { 99, 55, 203, 99, 4, 91 };
+        String string = "GloomySunday";
+        Set<Integer> set = new HashSet<>();
+
+         names.stream()
+                .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(n -> n.getValue() == 1)
+                .map(m -> m.getKey())
+                .findFirst().ifPresent(System.out::println);
+
+
+
+
+    }
 
 }
