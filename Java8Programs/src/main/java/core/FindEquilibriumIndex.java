d88d588e5cdfd3228e52290a5a37a439494e1076
shaj <shajashwin@gmail.com>
2025-02-20 19:14:22 +0530

    updated

---
+++ Java8Programs/src/main/java/core/FindEquilibriumIndex.java
@@ -0,0 +1,31 @@
+package core;
+
+public class FindEquilibriumIndex {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,1,2,3,5,6,-11};
+        System.out.println(findEquilibriumIndex(arr));
+
+    }
+
+    public static int findEquilibriumIndex(int[] arr){
+        int totalSum = 0;
+        for(int a : arr){
+            totalSum += a;
+        }
+
+        int leftSum = 0;
+        for (int i=0;i< arr.length;i++){
+            int rightSum = totalSum - leftSum - arr[i];
+
+            if(rightSum == leftSum) return arr[i];
+
+            leftSum += arr[i];
+        }
+
+        return -1;
+
+    }
+
+}
---
+++ Java8Programs/src/main/java/core/SwapNumberInArray.java
@@ -0,0 +1,25 @@
+package core;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//swap alternative elements in array
+public class SwapNumberInArray {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8};
+        int n = 7;
+        swapArray(n,array);
+
+    }
+
+    public static void swapArray(int n, int[] array){
+
+        for(int i=0;i<array.length-1;i+=2){
+            int temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
